a1333d2593ebf44bf1be455a695fab25e5874bf9
Dominik Mrówka <dmrowka7@gmail.com>
2022-03-25 10:00:22 +0100

    Add initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -87,7 +87,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
-      console.log('new Product:', thisProduct);
+      thisProduct.initAccordion();
     }
 
     renderInMenu() {
@@ -105,8 +105,41 @@
       /* add element to menu */
 
       menuContainer.appendChild(thisProduct.element);
+    };
+
+    initAccordion(){
+    const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log(clickableTrigger);
+      // const allClickableTrigger = document.querySelectorAll(select.menuProduct.clickable);
+      // for (let clickableTrigger of allClickableTrigger) {
+      //   console.log(clickableTrigger);
+      // }
+
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        console.log(activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct == thisProduct.element) {
+          activeProduct.classList.remove('active')
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
+
     }
   }
 
   app.init();
 }
+
+
+
